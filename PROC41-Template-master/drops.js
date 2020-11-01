5f7cc5ee76c3416606432c1b5934860aa1147f59
srushteeM <59560681+srushteeM@users.noreply.github.com>
2020-11-02 00:35:12 +0530

    Update drops.js
    Check the changes

--- PROC41-Template-master/drops.js
+++ PROC41-Template-master/drops.js
@@ -1,16 +1,24 @@
-class Drops{
+class createDrop{
     constructor(x,y){
         var options = {
-            'restitution':0.8,
-            'friction':1.0,
-            'density':1.0
+            friction: 0.001,
+            restitution:0.1           
         }
-    this.body=ellipse(x,y,options)
-}
-display(){
-    push()
-    elllipseMode(CENTER)
-    ellipse(this.body.position.x,this.body.position.y)
-    pop()
-}
+        this.rain = Bodies.circle(x,y,5,options)
+        this.radius = 5;
+        World.add(world, this.rain);
+    }
+
+    updateY(){     
+        if(this.rain.position.y > height){
+
+            Matter.Body.setPosition(this.rain, {x:random(0,400), y:random(0,400)})
+        }
+    }
+
+    showDrop(){
+        fill("blue")
+        ellipseMode(CENTER);
+        ellipse(this.rain.position.x,this.rain.position.y,this.radius,this.radius);
+    }
 }
